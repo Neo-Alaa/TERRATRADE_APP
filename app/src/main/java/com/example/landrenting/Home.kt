bd78e726b7d1c3c9688fa0407053834d4499bb45
Alaa Eddine <bendahanealaa@gmail.com>
2025-04-17 21:29:35 +0100

    Update Home Interface

--- app/src/main/java/com/example/landrenting/Home.kt
+++ app/src/main/java/com/example/landrenting/Home.kt
@@ -1,9 +1,6 @@
 package com.example.landrenting
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.View
 import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
@@ -11,11 +8,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Welcome : AppCompatActivity() {
+class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
-
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
@@ -24,12 +19,7 @@ class Welcome : AppCompatActivity() {
                 View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
                         View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 )
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, Home::class.java))
-            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-            finish()
-        }, 5000)
+        setContentView(R.layout.activity_home)
 
     }
 }
